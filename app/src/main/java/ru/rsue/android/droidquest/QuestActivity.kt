cb7167832ac186449d7a7fccc09e19de1eda6d4b
Maria <belldom666@yandex.ru>
2024-03-03 20:00:23 +0300

    Initial Commit

--- app/src/main/java/ru/rsue/android/droidquest/QuestActivity.kt
+++ app/src/main/java/ru/rsue/android/droidquest/QuestActivity.kt
@@ -2,6 +2,7 @@ package ru.rsue.android.droidquest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
@@ -9,6 +10,10 @@ import android.widget.Toast
 
 
 class QuestActivity : AppCompatActivity() {
+    companion object{
+        private val TAG = "QuestActivity"
+        private val KEY_INDEX = "index"
+    }
 
     private lateinit var mTrueButton: Button
     private lateinit var mFalseButton: Button
@@ -20,13 +25,24 @@ class QuestActivity : AppCompatActivity() {
         Question(R.string.question_linear, false),
         Question(R.string.question_service, false),
         Question(R.string.question_res, true),
-        Question(R.string.question_manifest, true)
+        Question(R.string.question_manifest, true),
+
+        Question(R.string.question_serviceone, false),
+        Question(R.string.question_manifesttwo, true),
+        Question(R.string.question_multitasking, false),
+        Question(R.string.question_graphic, true),
+        Question(R.string.question_saving, false)
     )
     private var mCurrentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quest)
+        Log.d(TAG, "onCreate(Bundle) вызван")
+
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
 
         mQuestionTextView = findViewById(R.id.question_text_view)
 
@@ -59,6 +75,12 @@ class QuestActivity : AppCompatActivity() {
         updateQuestion()
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.i(TAG, "onSaveInstanceState")
+        outState.putInt(KEY_INDEX, mCurrentIndex)
+    }
+
     private fun updateQuestion() {
         val question = mQuestionBank[mCurrentIndex].textResId
         mQuestionTextView.setText(question)
@@ -72,5 +94,26 @@ class QuestActivity : AppCompatActivity() {
             R.string.incorrect_toast
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() вызван")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() вызван")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() вызван")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() вызван")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() вызван")
     }
 }
